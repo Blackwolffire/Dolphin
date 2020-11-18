0f7ed1fb41730511e33d4231b43977c9667d89ad
Andre Debuisne <andre.debuisne@epita.fr>
2020-11-18 23:36:30 +0100

    main(helper): added helper functions

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import datetime
-
 import database
 import data
+from asset import Asset
+import matplotlib.pyplot as plt
+
 db = database.Database('new.sqlite')
-currencies = ['EUR', 'USD', 'JPY']
 
-start = datetime.datetime.strptime('2018-10-10', '%Y-%m-%d')
-end = datetime.datetime.strptime(data.END_DATE, '%Y-%m-%d')
-delta = datetime.timedelta(days=1)
 
-while start != end:
-    print(start)
-    for c1 in currencies:
-        for c2 in currencies:
-            if c1 != c2:
-                rate = data.get_currency_rate(c1, c2, start.strftime('%Y-%m-%d'))
-                db.add_currency(c1, c2, start, rate)
+def get_portfolio():
+    return data.get_portfolio(db.get_portfolio_asset())
 
-    start += delta
+
+def get_assets():
+    return db.get_assets()
+
+
+def get_df(asset: Asset, plot=False):
+    df = db.get_quotes([asset], start_date=data.START_DATE, end_date=data.END_DATE, data_frame=True)
+    if plot:
+        df.plot(y='close')
+        plt.show()
+    else:
+        return df
+
+
